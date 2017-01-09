4b5e833f7b60cf9d9c0451b3dc4a90f96a65c59e
Simon Kissane <skissane@gmail.com>
2017-01-09 20:15:59 +1100

    Provide reference to VHD on all statements
    Added to each statement output the reference:
    	imported from (S143)
    		Victorian Heritage Database (Q28152287)

--- getQuickStatements.sh
+++ getQuickStatements.sh
@@ -5,5 +5,5 @@
 # https://tools.wmflabs.org/wikidata-todo/quick_statements.php
 #
 ./getLackingStatus.sh | while read wikidataEntity vhdID; do
-	echo -e "$wikidataEntity\tP1435\t$(./getHeritageStatusID.sh "$(./getVictorianHeritageStatus.sh $vhdID)")"
+	echo -e "$wikidataEntity\tP1435\t$(./getHeritageStatusID.sh "$(./getVictorianHeritageStatus.sh $vhdID)")\tS143\tQ28152287"
 done
